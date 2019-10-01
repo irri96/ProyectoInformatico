f8aba8256e178db55434f82f703ba3ffc197fa79
Alexander Irribarra <irri96@live.cl>
2019-09-30 21:44:40 -0300

    quitar geodjango

--- Core/models.py
+++ Core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.gis.db import models as gmodels
 
 class User(AbstractUser):
     #se heredan los campos basicos (nombre, email, pass, etc) de AbstractUser
@@ -19,7 +18,8 @@ class Arrendador(models.Model):
 class Estacionamiento(models.Model):
     duenio = models.ForeignKey(Conductor,on_delete=models.CASCADE)
     #localizacion como punto en srid 4326 (GPS)
-    localizacion = gmodels.PointField()
+    lat = models.DecimalField(max_digits=10,decimal_places=7)
+    lon = models.DecimalField(max_digits=10, decimal_places=7)
 
 # class Valoracion(models.Model):
 #     emisor = models.ForeignKey(User,on_delete=models.CASCADE,related_name="emisor")
